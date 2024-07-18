506c416eab6f06f4edd3ac9ae1729110c9cf8581
Umutoni26 <vanessaumutoni26@gmail.com>
2024-07-17 19:23:35 -0700

    this the main page of the karrkelly project

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Landingpage from "./pages/Landingpage"
+import LoginSignup from "./pages/LoginSignup"
+import Product from "./pages/Product"
+import Cart from "./pages/Cart"
+
+
+
+function App() {
+
+  return (
+    <>
+      
+       <Router>
+        <Navbar/>
+        <Routes>
+           
+          <Route path='/' element={<Landingpage/>}/>
+          <Route path='product' element={<Product/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+       </Router>
+    </>
+  )
+}
+
+export default App
+
+// import Navbar from './components/Navbar'
+// import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// // import Home from './pages/Home';
+// import LoginSignup from './pages/LoginSignup';
+// import Product from './pages/Product';
+// import Cart from './pages/Cart';
+// import Service from './pages/Service';
+// import Contactus from './pages/Contactus';
+// import Aboutus from './pages/Aboutus';
+
+// import './index.css';
+// import Landingpage from './pages/Landingpage';
+
+
+// function App() {
+  
+
+//   return (
+   
+//    <Router>
+      
+//       <Navbar/>
+//       <Routes>
+//       <Route path='/' element={<Landingpage/>}/>
+//       <Route path='/login' element={<LoginSignup/>}/>
+//       <Route path='/product' element={<Product/>}/>
+//       <Route path='/cart' element={<Cart/>}/>
+//       <Route path='/Service' element={<Service/>}/>
+//       <Route path='/Contact' element={<Contactus/>}/>
+//       <Route path='/About' element={<Aboutus/>}/>
+
+      
+//       </Routes>
+
+//    </Router>
+   
+  
+   
+    
+//   )
+// }
+
+// export default App
+
---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Service from '../components/Service'
+import Aboutus from '../components/Aboutus'
+import Contactus from '../components/Contactus'
+import Footer from '../components/Footer'
+
+const Landingpage = () => {
+  return (
+    <>
+      <Hero/>
+      <Service/>
+      <Aboutus/>
+      <Contactus/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Landingpage
